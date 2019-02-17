e66bae5409d0c11b7a6cedcc3884f4faff58af19
Icaro Rezende <icaronr@gmail.com>
2019-02-16 23:48:18 -0200

    fixing ajax test and added btc credentials

--- spec/system/exchanges_index_system_spec.rb
+++ spec/system/exchanges_index_system_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe "Exchange Currency Process", :type => :system, js: true do
       select('USD', from: 'target_currency')
       fill_in 'amount', with: '10'
     end
-    click_button 'CONVERTER'
- 
+    #click_button 'CONVERTER' trocado para alterar ao entrar com input
+    wait_for_ajax
     # save_and_open_page
-    expect(page).to have_content("value")
+    expect("#result").to change.by(1)
   end
 end
